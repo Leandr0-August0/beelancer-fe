4f77be5df3fe401d2ccbdf368017f4af66e57953
Leandr0-August0 <leco.muniz012@gmail.com>
2025-05-20 19:04:51 -0300

    resolução da problema do token inexistente

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,23 +1,24 @@
 export const axiosConfig = {
     headers: {
         authorization: `Bearer ${
-            typeof window !== 'undefined'
-                ? JSON.parse(sessionStorage.getItem('authToken')).token
+            typeof window !== "undefined" 
+            && sessionStorage.getItem("authToken")
+                ? JSON.parse(sessionStorage.getItem("authToken")).token
                 : null
         }`,
     },
 };
 
 export const verifyToken = (returnToken) => {
-    const token = sessionStorage.getItem('authToken');
+    const token = sessionStorage.getItem("authToken");
 
     if (!token) return null;
 
     const { expiresIn } = JSON.parse(token);
 
     if (Date.now() > expiresIn) {
-        sessionStorage.removeItem('authToken');
-        alert('Seu token expirou, faça login novamente');
+        sessionStorage.removeItem("authToken");
+        alert("Seu token expirou, faça login novamente");
         return null;
     }
 
@@ -28,8 +29,8 @@ export const authService = (router, ok) => {
     const token = verifyToken();
 
     if (!token) {
-        router.push('/login');
-        return alert('Você precisa estar logado para acessar essa página');
+        router.push("/login");
+        return alert("Você precisa estar logado para acessar essa página");
     }
 
     ok();
@@ -37,12 +38,12 @@ export const authService = (router, ok) => {
 
 export const logout = (router) => {
     {
-        typeof window !== 'undefined' && sessionStorage.removeItem('authToken');
+        typeof window !== "undefined" && sessionStorage.removeItem("authToken");
     }
 
-    router.push('/');
+    router.push("/");
 };
 
 export const login = (router) => {
-    router.push('/login');
+    router.push("/login");
 };
